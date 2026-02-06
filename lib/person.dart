5836deeeef543d4404ff433ea29bd84106f115e9
Silas HAKUZWIMANA <hakusilas@gmail.com>
2026-02-06 19:25:07 +0200

    Refactor Person class structure and methods

--- lib/person.dart
+++ lib/person.dart
@@ -1,8 +1,10 @@
 class Person {
   final String name;
 
+  // Constructor to initialize person's name
   Person(this.name);
 
+  // Method to introduce the person
   void introduce() {
     print("My name is $name");
   }
