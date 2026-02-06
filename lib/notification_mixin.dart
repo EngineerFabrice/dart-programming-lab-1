41a9cf8315e32035745ae28591c05931697d3fb6
Silas HAKUZWIMANA <hakusilas@gmail.com>
2026-02-06 19:24:56 +0200

    Improve notification mixin functionality

--- lib/notification_mixin.dart
+++ lib/notification_mixin.dart
@@ -1,4 +1,5 @@
 mixin NotificationMixin {
+  // Method to notify registration success
   void notifyRegistrationSuccess(String studentName, String courseName) {
     print("Notification: $studentName Successfully registered for $courseName");
   }
